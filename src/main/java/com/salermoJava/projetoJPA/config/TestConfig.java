1c221259f5b5915d6890e987aa8e2f0b802b32b1
gbsalermo <enzogabrielskull@gmail.com>
2026-01-31 19:48:39 -0300

    Injeção de dependencia e JPA Repository

---
+++ src/main/java/com/salermoJava/projetoJPA/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.salermoJava.projetoJPA.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.salermoJava.projetoJPA.entities.User;
+import com.salermoJava.projetoJPA.repositories.UserRepository;
+
+@Configuration //Anotação que informa que ela é uma classe de configuração
+@Profile("test") 
+public class TestConfig implements CommandLineRunner {
+	
+	//Injeção de dependencia
+	@Autowired //Ele que vai fazer essa dependencia, indicando onde a injeção vai ser automaticamente aplicada
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception { 
+	//Esse metodo run, de CommandLineRunner, serve para tudo que colocar dentro dele seja imediatamente executado quando a aplicação for iniciada
+		//Id nulo pois vai ser gerado pelo proprio banco de dados
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2)); //Uso o saveAll para salvar os objts u1 e u2 como lista usando o Arrays.asList
+
+			
+	}
+}
---
+++ src/main/java/com/salermoJava/projetoJPA/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.salermoJava.projetoJPA.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salermoJava.projetoJPA.entities.User;
+
+//Não preciso adicionar uma implementação pois o SpringJPA ja tem uma padrão
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
