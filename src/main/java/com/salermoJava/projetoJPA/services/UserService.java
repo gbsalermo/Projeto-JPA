731b1e9f883c8f9fafe97460184458c1734aeb36
gbsalermo <enzogabrielskull@gmail.com>
2026-02-01 23:03:46 -0300

    Delete Exception

--- src/main/java/com/salermoJava/projetoJPA/services/UserService.java
+++ src/main/java/com/salermoJava/projetoJPA/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.salermoJava.projetoJPA.entities.User;
 import com.salermoJava.projetoJPA.repositories.UserRepository;
+import com.salermoJava.projetoJPA.services.exceptions.DatabaseException;
 import com.salermoJava.projetoJPA.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id); //Excessão para caso tente deletar um usuario que não existe 
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id); //O getOne mudou, entao pode aparecer como getReferenceById
