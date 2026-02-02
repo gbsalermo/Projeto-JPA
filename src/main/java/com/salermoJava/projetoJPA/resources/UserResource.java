e9b1f2d36be39d30dbdb7a71f401d739c5faf95f
gbsalermo <enzogabrielskull@gmail.com>
2026-02-01 21:11:43 -0300

    Adicionado o User Insert

--- src/main/java/com/salermoJava/projetoJPA/resources/UserResource.java
+++ src/main/java/com/salermoJava/projetoJPA/resources/UserResource.java
@@ -1,16 +1,21 @@
 package com.salermoJava.projetoJPA.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.salermoJava.projetoJPA.entities.User;
 import com.salermoJava.projetoJPA.services.UserService;
+
 
 //Faz parte da camada de recursos
 
@@ -33,5 +38,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		//Uso o URI que serve para identificar e localizar(Recursos/entidades) funcionando como uma etiqueta
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
